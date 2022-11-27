9b67ce87b741ebfa7d3dd0dfe211e6c51e460c45
Chris <chrisdietrich3666@gmail.com>
2022-11-27 06:13:52 -0500

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,9 +2,12 @@
 
 import build from "next/dist/build"
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export const handler(req, res) {
+  res.status(200).send("hello")
 }
+
+
+
 // hello . js is an endpoint, Nextjs provides us the ability to build
 // endpoints with not a lot of code. Next js replaces node js and express 
 // it's a form of abstraction away from node and express
