8d7c0e263aa96bbfe02d8aa3bd09faca14607dc8
Chris <chrisdietrich3666@gmail.com>
2022-12-26 17:15:57 -0500

    feat added services checkbox functionality

--- components/ServicesCheckbox/index.jsx
+++ components/ServicesCheckbox/index.jsx
@@ -1,36 +1,75 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
-const ServicesCheckbox = ({isSubmitting, change}) => {
-  
-  const func = (e) => {
-    e.preventDefault();
-    console.log(e);
+const ServicesCheckbox = ({ isSubmitting, change }) => {
+  const [lawnCare, setLawnCare] = useState(false);
+  const [powerWashing, setPowerWashing] = useState(false);
+  const [stumpRemoval, setStumpRemoval] = useState(false);
+  const [snowRemoval, setSnowRemoval] = useState(false);
+
+  const filteredServices = (services) => {
+    const filteredServices = Object.entries(services);
+    return filteredServices.filter((service) =>
+      service[1] === true).map(service => service[0] )
   };
+
+  useEffect(() => {
+    const services = {
+      "Lawn Care": lawnCare,
+      "Powerwashing": powerWashing,
+      "Stump Removal": stumpRemoval,
+      "Snow Removal": snowRemoval,
+    };
+    change(filteredServices(services));
+  }, [lawnCare, powerWashing, stumpRemoval, snowRemoval]);
 
   return (
     <div>
       <h3 className={styles.checkbox_title}>
         What services are you interested in?
       </h3>
-      <form className={styles.checkbox_container} onSubmit={func}>
+      <form className={styles.checkbox_container}>
         <div>
           <div>
-            <input name="lawn-care" type="checkbox" id="lawn-care" />
+            <input
+              value={lawnCare}
+              onChange={(e) => setLawnCare((prevState) => !prevState)}
+              name="lawn-care"
+              type="checkbox"
+              id="lawn-care"
+            />
             <label htmlFor="lawn-care">Lawn Care</label>
           </div>
           <div>
-            <input name="power-washing" type="checkbox" id="power-washing"/>
+            <input
+              value={powerWashing}
+              onChange={(e) => setPowerWashing((prevState) => !prevState)}
+              name="power-washing"
+              type="checkbox"
+              id="power-washing"
+            />
             <label htmlFor="power-washing">Power Washing</label>
           </div>
         </div>
         <div>
           <div>
-            <input name="stump-removal" type="checkbox" id="stump-removal" />
+            <input
+              value={stumpRemoval}
+              onChange={(e) => setStumpRemoval((prevState) => !prevState)}
+              name="stump-removal"
+              type="checkbox"
+              id="stump-removal"
+            />
             <label htmlFor="stump-removal">Stump Removal</label>
           </div>
           <div>
-            <input name="snow-removal" type="checkbox" id="snow-removal"/>
+            <input
+              value={snowRemoval}
+              onChange={(e) => setSnowRemoval((prevState) => !prevState)}
+              name="snow-removal"
+              type="checkbox"
+              id="snow-removal"
+            />
             <label htmlFor="snow-removal">Snow Removal</label>
           </div>
         </div>
@@ -41,80 +80,3 @@ const ServicesCheckbox = ({isSubmitting, change}) => {
 
 export default ServicesCheckbox;
 
-// import React, { Component } from "react";
-// import axios from "axios";
-// import styles from "./styles.module.css";
-
-// class ServicesCheckbox extends Component {
-//   state = {
-//     value: [],
-//     serviceItems: [],
-//     isFormSubmitting: false,
-//   };
-
-//   async componentDidMount() {
-//     const { data } = await axios.get("http://localhost:3001/api/getServices");
-//     this.setState({ ...this.state, serviceItems: data.services });
-//   }
-
-//   async shouldComponentUpdate(nextProps) {
-//     if (nextProps.isSubmitting && !this.state.isFormSubmitting) {
-//       this.setState({ isFormSubmitting: true });
-//     }
-
-//     if (!nextProps.isSubmitting && this.state.isFormSubmitting) {
-//       this.setState({ isFormSubmitting: false, value: [] });
-//       this.props.change([]);
-
-//       document
-//         .querySelectorAll("input[type=checkbox]")
-//         .forEach((el) => (el.checked = false));
-//     }
-//   }
-
-//   onChange = (e) => {
-//     const isChecked = this.state.value.find((element) => element === e.target.value)
-//     let services = []
-//     if (!isChecked) {
-//       services = [...this.state.value, e.target.value];
-//     }
-//     else {
-//       services = this.state.value.filter(
-//         (element) => element !== e.target.value
-//       );
-//     }
-//     this.setState({
-//       value: services
-//     })
-//     this.props.change(services)
-//   };
-
-//   render() {
-//     return (
-//       <div className="checkbox-container">
-//         <h3>What services are you interested in?</h3>
-
-//         <div className="service-container">
-//           {this.state.serviceItems.map((service, index) => {
-//             return (
-//               <div className={styles.services_container} key={index}>
-//                 <label className="contact-label">
-//                   <input
-//                     disabled={this.props.isSubmitting}
-//                     type="checkbox"
-//                     value={service.permalink}
-//                     className="checkbox-background-color"
-//                     onChange={this.onChange}
-//                   />
-//                   <span className={styles.service_name}>{service.name}</span>
-//                 </label>
-//               </div>
-//             );
-//           })}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default ServicesCheckbox;
--- pages/contact.js
+++ pages/contact.js
@@ -33,20 +33,20 @@ export default class Contact extends React.Component {
     };
   }
 
-  async componentDidMount() {
-    const services = {
-      services: [
-        { id: 1, name: "Lawn Care", permalink: "lawn-care" },
-        { id: 2, name: "Power Washing", permalink: "power-washing" },
-        { id: 3, name: "Snow Removal ", permalink: "snow-removal" },
-        { id: 4, name: "Stump Removal", permalink: "stump-removal" },
-        { id: 5, name: "Demo & Hauling", permalink: "demolition" },
-        { id: 6, name: "Regrades", permalink: "regrades" },
-      ],
-    };
-    // const { data } = await axios.get("http://localhost:3001/api/getServices");
-    this.setState({ ...this.state, services });
-  }
+  // async componentDidMount() {
+  //   // const services = {
+  //   //   services: [
+  //   //     { id: 1, name: "Lawn Care", permalink: "lawn-care" },
+  //   //     { id: 2, name: "Power Washing", permalink: "power-washing" },
+  //   //     { id: 3, name: "Snow Removal ", permalink: "snow-removal" },
+  //   //     { id: 4, name: "Stump Removal", permalink: "stump-removal" },
+  //   //     { id: 5, name: "Demo & Hauling", permalink: "demolition" },
+  //   //     { id: 6, name: "Regrades", permalink: "regrades" },
+  //   //   ],
+  //   // };
+  //   // const { data } = await axios.get("http://localhost:3001/api/getServices");
+  //   // this.setState({ ...this.state, services });
+  // }
 
   render() {
     return (
@@ -109,7 +109,8 @@ export default class Contact extends React.Component {
               this.setState({ service: targetValue });
             }}
           />
-
+          
+          {JSON.stringify(this.state)}
           <div className={styles.contact_form}>
             {"service" in this.state.error && (
               <p className="error-message">
