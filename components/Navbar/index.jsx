63ebf286737f222fa3d034e91448715555c4acb7
Chris <chrisdietrich3666@gmail.com>
2022-12-30 19:58:12 -0500

    feat styled navbar and slideshow

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+// import { GiHamburgerMenu } from "react-icons/gi";
+import { BsFacebook } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
+
+import styles from "./styles.module.css";
 
 const Navbar = () => {
-
   const services = [
     { title: "Lawn Care", permalink: "lawn-care" },
     { title: "Power Washing", permalink: "power-washing" },
@@ -12,25 +19,48 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md">
       <div className="container-fluid">
-        <a href="/" className="navbar-brand">
-          <img
-            src="/images/logo.jpg"
-            alt="Dietrich Landcare"
-          />
-        </a>
+        <Link href="/">
+          <a className="navbar-brand mr-4">
+            <Image
+              src="/images/logo.jpg"
+              width="50px"
+              height="50px"
+              alt="logo"
+            />
+          </a>
+        </Link>
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <Link href="/">
+                <a className="nav-link" aria-current="page">
+                  Home
+                </a>
+              </Link>
             </li>
             {services && (
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   Services
                 </a>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -50,17 +80,40 @@ const Navbar = () => {
               </li>
             )}
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/testimonials">Testimonials</a>
+              <Link href="/testimonials">
+                <a className="nav-link" aria-current="page">
+                  Testimonials
+                </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/contact">Contact</a>
+              <Link href="/contact">
+                <a className="nav-link" aria-current="page">
+                  Contact
+                </a>
+              </Link>{" "}
             </li>
           </ul>
+          <div className={styles.social_icons_container}>
+            <a
+              href="https://www.facebook.com/Dietrich-Land-Care-LLC-571934750168436"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFacebook className={styles.social_media_icon} />
+            </a>
+            <a
+              href="https://www.instagram.com/dietrich_landcarellc/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillInstagram className={styles.social_media_icon} />
+            </a>
+          </div>
         </div>
       </div>
     </nav>
   );
-
-}
+};
 
 export default Navbar;
